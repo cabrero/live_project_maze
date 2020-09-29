5bca9ce8d720aa77e9dc8c896a499486d8d4c295
David Cabrero <david.cabrero@madsgroup.org>
2020-09-29 19:18:05 +0200

    Added code for reading maze data from file

--- solving_a_maze.py
+++ solving_a_maze.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+
+
+import sys
+
+filepath = sys.argv[1]
+
+begin = 'o'
+end   = 'x'
+wall  = '*'
+
+# Read maze data from file
+with open(filepath, 'r') as f:
+    maze = [list(line.strip().lower()) for line in f if not line.isspace()]
+begin_xy = None
+end_xy = None
+
+# Check maze data 
+assert maze
+cols = len(maze[0])
+for y, row in enumerate(maze):
+    assert len(row) == cols
+    if begin in row:
+        begin_xy = (row.index(begin), y)
+    if end in row:
+        end_xy = (row.index(end), y)
+assert begin_xy
+assert end_xy
+
+
